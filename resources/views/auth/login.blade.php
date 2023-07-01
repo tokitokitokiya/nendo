7aa456c123442cd753106bfaf051ed8ccafafa6d
tokitokitokiya <tokitokitokiyahime@yahoo.co.jp>
2023-07-01 16:44:45 +0900

    login画面に登録画面へのリンクを実装

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -38,6 +38,10 @@
                     {{ __('Forgot your password?') }}
                 </a>
             @endif
+            <p>&emsp;&emsp;</p>
+            <a class="underline text-sm text-gray-600 hover:text-gray-900 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500" href="/register">
+                    {{ __('register(登録する)') }}
+            </a>
 
             <x-primary-button class="ml-3">
                 {{ __('Log in') }}
